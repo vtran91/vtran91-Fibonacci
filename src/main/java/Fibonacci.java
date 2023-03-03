0712a35b699feb3c1eb6eab9e0299c1b0c6401d0
vtran91 <vtran16@bellsouth.net>
2023-03-03 05:04:32 +0000

    Completed Fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -15,6 +15,17 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        int n1 = 0; int n2 = 1; int n3 = 0;
+
+        if (n < 2)
+            return n2;
+
+        for (int i = 1; i < n; i++)
+        {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+        return n3;
     }
 }
